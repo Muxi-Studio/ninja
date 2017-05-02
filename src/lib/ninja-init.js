c88efe20d4366aca669160e7a4861d3d32cba239
stephenliu <stephenlyao@yeah.net>
2017-05-02 11:14:12 +0800

    throw error

--- src/lib/ninja-init.js
+++ src/lib/ninja-init.js
@@ -6,6 +6,11 @@
  * Scaffolding webpack or gulp based project using Slush
  */
 
+const fs = require('fs');
+const path = require('path');
+const chalk = require('chalk');
+
+
 module.exports = function() {
 	console.log("ninja init!!");
 }
